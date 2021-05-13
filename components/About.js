1e439feaadc4d274a83fed1e9ea87cb0f202cfa3
syket-git <syketb@gmail.com>
2021-05-13 20:25:12 +0600

    about added

---
+++ components/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from '../styles/About.module.css';
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <div className={styles.about__details}>
+        <p>
+          Hi! I'm Syket Bhattachergee, a dedicated Web Developer. I develop web
+          applications. My core skill is based on JavaScript and I love to do
+          most of the things using JavaScript. I have graduated with a diploma
+          degree in Computer Science Technology from Chittagong Polytechnic
+          Institute at Chittagong. I am available for any kind of job
+          opportunity that suits my interests.
+        </p>
+        <div className={styles.about__cta}>
+          <button className={styles.resume}>Get Resume</button>
+          <button className={styles.skills}>My Skills</button>
+        </div>
+      </div>
+      <div className={styles.about__img}>
+        <img src="/syket.jpg" alt="Syket Bhattachergee" />
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Layout from '../components/Layout';
 import Home from '../components/Home';
+import About from '../components/About';
 
 export default function Index() {
   return (
     <Layout>
       <div>
         <Home />
+        <About />
       </div>
     </Layout>
   );
